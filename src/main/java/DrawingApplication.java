597c64bcbdb13f2818d5e0e4320e66a13c382bdc
RoberteFlaherty <45772389+RoberteFlaherty@users.noreply.github.com>
2020-10-20 16:20:22 -0400

    tutorial 7 injecting objects

--- src/main/java/DrawingApplication.java
+++ src/main/java/DrawingApplication.java
@@ -1,5 +1,7 @@
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApplication {
@@ -7,8 +9,10 @@ public class DrawingApplication {
     public static void main(String[] args)
     {
         //Triangle triangle = new Triangle();
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/main/resources/spring.xml"));
-        Triangle triangle = (Triangle)factory.getBean("triangle");
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/main/resources/spring.xml"));
+        ApplicationContext ac = new ClassPathXmlApplicationContext("file:src/main/resources/spring.xml");
+        Triangle triangle = (Triangle)ac.getBean("triangle");
+        // calling triangle classes draw method
         triangle.draw();
 
     }
